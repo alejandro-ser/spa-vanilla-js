5ce005aa09aca4fbc39837b3f42b17490cd56451
Alejo <alejosegi@gmail.com>
2020-05-30 00:10:19 +0200

    Instalacion webpack-html-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,14 @@
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
 module.exports = {
     entry: './src/main.js',
     output: {
         path: __dirname + '/dist',
         filename: 'bundle.js'
-    }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
 }
